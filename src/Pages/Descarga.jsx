d28bc597ec9bf7db97c20a208e75bb2a3f6456a1
Wilder <wilderomarddd@gmail.com>
2024-10-16 15:08:10 -0500

    Commit de prueba

--- src/Pages/Descarga.jsx
+++ src/Pages/Descarga.jsx
@@ -12,7 +12,7 @@ const Descarga = () => {
         <>
             <Cabecera/>
             <main className='flex flex-col gap-5 md:h-[550px] md:overflow-y-auto md:bg-[#212121] md:ml-[210px]'>
-                <span className='px-4 font-[500] text-[18px] mt-2'>Descargar contenido 4-0</span>
+                <span className='px-4 font-[500] text-[18px] mt-2'>Descargar contenido wilder commit</span>
                 <C_input/>
                 <C_eleccion/>
                 <Caja_Recientes cancion="cancion2"/>
